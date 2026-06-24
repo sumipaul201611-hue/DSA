07311239fa6187370c9e4ede1faaf0d7746498a4
sumipaul201611-hue <sumipaul201611@gmail.com>
2026-06-24 15:57:15 +0600

    Time: 0 ms (100.00%) | Memory: 31.4 MB (7.08%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
+
+    int binarySearch(vector<int>&nums,int target,int l,int r){
+        int mid=l+(r-l)/2;
+
+        if(l<=r){
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                return binarySearch(nums,target,mid+1,r);
+            }
+            else{
+                  return binarySearch(nums,target,l,mid-1);
+            }
+        }
+        return -1;
+    }
     int search(vector<int>& nums, int target) {
          
-           int l=0;
-           int r=nums.size()-1;
-
-           while(l<=r){
-            int mid=l+(r-l)/2;
-              
-              if(nums[mid]==target){
-                 return mid;
-              }
-
-              else if(nums[mid]<target){
-                 l=mid+1;
-              }
-              else{
-                r=mid-1;
-              }
-           }
-
-           return -1;
+         return binarySearch(nums,target,0,nums.size()-1);
     }
 };
